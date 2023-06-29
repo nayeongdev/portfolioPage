c37cf093b04fd2ad93d8e137353070e536664d9a
nayeongdev <nayeongkim.dev@gmail.com>
2023-06-29 17:10:03 +0900

    탑부분 스크롤 이벤트 수정:숨기기->반투명

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 const $ = (selector) => document.querySelector(selector);
 const body = document.body;
 
+// 메뉴 동적 활성화
 const navItems = document.querySelectorAll(".nav-item");
 const sections = document.querySelectorAll(".section");
 
@@ -16,31 +17,16 @@ function changeLinkState() {
 changeLinkState();
 window.addEventListener("scroll", changeLinkState);
 
-// function hasScrolled() {
-//   const scrollY = window.scrollY;
-//   const scrollDown = scrollY > lastScrollY;
-//   if (scrollDown) {
-//     $(".header").classList.add("hide");
-//   } else {
-//     $(".header").classList.remove("hide");
-//   }
-//   lastScrollY = scrollY;
-// }
+// 헤더부분 반투명으로 변환
+window.onscroll = function() {scrollFunction()};
 
-// // 스크롤 감지
-// let didScroll;
-// let lastScrollY = 0;
-
-// // 스크롤 이벤트
-// window.onscroll = (e) => (didScroll = true);
-
-// // 0.25초마다 스크롤 여부를 체크하여 스크롤 중이면 hasScrolled() 호출
-// setInterval(() => {
-//   if (didScroll) {
-//     hasScrolled();
-//     didScroll = false;
-//   }
-// }, 250);
+function scrollFunction() {
+  if (body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $(".header").classList.add("semi-transparent");
+  } else {
+    $(".header").classList.remove("semi-transparent");
+  }
+}
 
 // 스크롤 상태바
 window.addEventListener("scroll", function () {
@@ -50,50 +36,6 @@ window.addEventListener("scroll", function () {
   let width = (gauge / (스크롤높이 - 높이)) * 100;
   document.querySelector(".scroll-gauge").style.width = width + "%";
 });
-
-// // 스크롤 감지
-// let lastScrollTop = 0;
-// let delta = 5;
-// let fixBox = $(".header");
-// let didScroll;
-
-// // 스크롤 이벤트
-// window.onscroll = (e) => (didScroll = true);
-
-// // 0.25초마다 스크롤 여부를 체크하여 스크롤 중이면 hasScrolled() 호출
-// setInterval(() => {
-//   if (didScroll) {
-//     hasScrolled();
-//     didScroll = false;
-//   }
-// }, 250);
-
-// function hasScrolled() {
-//   let nowScrollTop = window.scrollY;
-//   const downCondition =
-//     nowScrollTop > lastScrollTop && nowScrollTop > fixBox.offsetHeight;
-//   const upCondition = nowScrollTop + window.innerHeight < body.offsetHeight;
-//   console.log(
-//     "nowScrollTop " + nowScrollTop + "\n" +
-//       "downCondition " + downCondition + "\n" +
-//       "upCondition " + upCondition
-//   );
-//   if (Math.abs(lastScrollTop - nowScrollTop) <= delta) {
-//     console.log(ping);
-//     return;
-//   }
-
-//   if (downCondition) {
-//     //scroll down
-//     fixBox.classList.remove("hide");
-//   } else {
-//     if (upCondition) {
-//       //scroll up
-//       fixBox.classList.add("hide");
-//     }
-//   }
-//   lastScrollTop = nowScrollTop;
-// }
 
 // 모바일용 메뉴 버튼
 $(".btn-menu-open").onclick = function () {
